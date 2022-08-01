80cc0b813f9c2a8261e659cf5ad015cac339e9f0
himikp <98259554+himikp@users.noreply.github.com>
2022-08-01 15:02:01 +0300

    Add files via upload

---
+++ convert/convert.py
@@ -0,0 +1,35 @@
+from moviepy.editor import *
+import os
+import requests
+import wget
+
+video_url= input("Enter url for video to convert: ")
+
+def download_video(url=''):
+    
+    try:    
+        response = requests.get(url=url, stream=True)
+               
+        with open('req_video.mp4', 'wb') as file:
+            for chunk in response.iter_content():
+                if chunk:
+                    file.write(chunk)
+            
+        return 'Video successfully downloaded!'
+    
+    except Exception as _ex:
+        return 'Check the URL please!'
+
+def video_to_gif():
+    clip = (VideoFileClip("req_video.mp4"))
+    clip.write_gif("output.gif", fps = 2)
+    print (os.getcwd() + f"\output.gif")
+
+
+def main():
+    download_video(video_url)
+    video_to_gif()
+    
+    
+if __name__ == "__main__":
+    main()
